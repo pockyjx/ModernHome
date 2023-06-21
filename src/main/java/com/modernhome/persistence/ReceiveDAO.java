73f082a0c9ac18430d8217e9a2b7368ef6e9d857
82105 <82105@KIM_GYURI>
2023-06-21 20:12:14 +0900

    Fix : 발주 관리 페이지 추가,수정 구현

--- src/main/java/com/modernhome/persistence/ReceiveDAO.java
+++ src/main/java/com/modernhome/persistence/ReceiveDAO.java
@@ -11,5 +11,14 @@ public interface ReceiveDAO {
 		
 	// 입고 조회 + 검색
 	public List<ReceiveVO> getReceiveSearch(String startDate, String endDate, String ma_name, String io_num);
-
+	
+	// 입고 등록
+	public void regReceive(ReceiveVO rvo);
+	
+	// 입고 수정
+	public void updateReceive(ReceiveVO rvo);
+	
+	// 입고 삭제
+	public void deleteReceive(int rec_id);
+	
 }
