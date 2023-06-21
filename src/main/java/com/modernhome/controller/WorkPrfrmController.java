7a69e07af56e08fe2093b6d6067342810e047a8f
leesr94 <sr940718@gmail.com>
2023-06-21 15:38:55 +0900

    Feat : 생산실적 컨트롤러, VO 생성 및 수정

---
+++ src/main/java/com/modernhome/controller/WorkPrfrmController.java
@@ -0,0 +1,37 @@
+package com.modernhome.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/production/performance")
+public class WorkPrfrmController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(WorkPrfrmController.class);
+	
+	// ===========================================
+	
+	
+	// 의존성 주입
+	
+	
+	// ===========================================
+	
+	
+	// 생산실적 목록 출력 (+ 검색)
+	
+	
+	// ===========================================
+	
+	
+	// 생산실적 추가 & 수정
+	
+	
+	// ===========================================
+	
+	
+	// 생산실적 삭제
+	
+}
